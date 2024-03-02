f8a3b4b0a4a6deb3cf481ddcfd430afead9bae7a
Ben Nguyen Tran <hnguyentran03@gmail.com>
2024-03-01 22:25:06 -0500

    finished about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ import { getImageUrl } from '../../utils';
 
 export const About = () => {
   return (
-    <section className={styles.container}>
+    <section className={styles.container} id='about'>
         <h2 className={styles.title}>About</h2>
         <div className={styles.content}>
             <img src={getImageUrl('about/aboutImage.png')} alt='About Image' className={styles.aboutImage}/>
@@ -13,7 +13,7 @@ export const About = () => {
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl('about/cursorIcon.png')} alt='Cursor Icon'/>
                     <div className={styles.aboutItemText}>
-                        <h1>Croaker</h1>
+                        <h3>Croaker</h3>
                         <p>
                             I am a major croaker
                         </p>
@@ -22,7 +22,7 @@ export const About = () => {
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl('about/serverIcon.png')} alt='Server Icon'/>
                     <div className={styles.aboutItemText}>
-                        <h1>Sobber</h1>
+                        <h3>Sobber</h3>
                         <p>
                             I am a major sobber
                         </p>
@@ -31,7 +31,7 @@ export const About = () => {
                 <li className={styles.aboutItem}>
                     <img src={getImageUrl('about/uiIcon.png')} alt='UI Icon'/>
                     <div className={styles.aboutItemText}>
-                        <h1>Fired</h1>
+                        <h3>Fired</h3>
                         <p>
                             I am fired
                         </p>
