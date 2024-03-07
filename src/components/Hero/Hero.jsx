baf29c9d1a8b5b249fb5953ab4a49d47f78296a2
Ben <hnguyent@andrew.cmu.edu>
2024-03-07 05:49:36 -0500

    updated skills and project images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     <section className={styles.container}>
         <div className={styles.content}>
             <h1 className={styles.title}>Hello, I'm Ben</h1>
-            <p className={styles.description}>I am croaking</p>
+            <p className={styles.description}>I am Computer Science student at Carnegie Mellon University</p>
             <a href="mailto:hnguyentran03@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
         <img src={getImageUrl('hero/heroImage.png')} alt='Hero Image' className={styles.heroImg}/>
