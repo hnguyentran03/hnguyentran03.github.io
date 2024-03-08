581df6fee9b07b5001b61520ee713cb11623ee83
Ben <hnguyent@andrew.cmu.edu>
2024-03-08 13:42:16 -0500

    reorded projects and added dates

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import styles from './ProjectCard.module.css';
 import { getImageUrl } from '../../utils';
 
-export const ProjectCard = ({ project: {title, imageSrc, description, skills, demo, source } }) => {
+export const ProjectCard = ({ project: {title, date, imageSrc, description, skills, demo, source } }) => {
   return (
     <div className={styles.container}>
         <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`} className={styles.image}/>
     
         <h3 className={styles.title}>{title}</h3>
+        <h4 className={styles.date}>{date}</h4>
         <p className={styles.description}>{description}</p>
         <ul className={styles.skills}>
             {
