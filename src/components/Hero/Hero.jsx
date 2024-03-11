dc830c88783a80d354dec362450faa20b3cb9ef1
Ben <hnguyent@andrew.cmu.edu>
2024-03-11 17:46:25 -0400

    added the typing rotating text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,68 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  const [iteration, setIteration] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const rotation = ["Croaker", "Computer Science student", "Software Engineer"];
+  const period = 1000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = iteration % rotation.length;
+    let fullText = rotation[i];
+    let newFullText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(newFullText);
+    console.log(newFullText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && newFullText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && newFullText === "") {
+      setIsDeleting(false);
+      setIteration(iteration + 1);
+      setIndex(1);
+      setDelta(200);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hello, I'm Ben</h1>
         <p className={styles.description}>
-          I am a Computer Science student at Carnegie Mellon University
+          {" "}
+          I am a{" "}
+          <span
+            data-period="1000"
+            data-rotate='["Croaker", "Computer Science student", "Software Engineer"]'
+          >
+            {text}
+          </span>
         </p>
         <a href="mailto:hnguyentran03@gmail.com" className={styles.contactBtn}>
           Contact Me
